ec371978fcf3415d3a027d4562319f205861bc51
7laslan <73464685+7laslan@users.noreply.github.com>
2021-11-15 13:04:44 +0200

    Add files via upload

---
+++ src/main/java/com/example/foodorderingapplication/FoodorderingapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodorderingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodorderingapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodorderingapplicationApplication.class, args);
+	}
+
+}
