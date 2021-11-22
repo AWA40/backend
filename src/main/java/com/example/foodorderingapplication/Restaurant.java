626e2d78c21ed9a046837452a1ac72c3aad006ec
7laslan <73464685+7laslan@users.noreply.github.com>
2021-11-22 14:00:57 +0200

    Update Restaurant.java

--- src/main/java/com/example/foodorderingapplication/Restaurant.java
+++ src/main/java/com/example/foodorderingapplication/Restaurant.java
@@ -2,20 +2,57 @@ package com.example.foodorderingapplication;
 
 import java.util.List;
 
-public class Restaurant {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-    private String restaurantName, address;
+@Entity
+@Table(name = "restaurant")
+public class Restaurant {
+    
+    @Column(name = "restaurant_name")
+    private String restaurantName;
+    
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "operating_hours")
     private long operatingHours;
+    
+    @Enumerated(EnumType.STRING)
     private Type type;
+    
+    @Column(name = "menu")
     private List<Product> menu;
 
-    public Restaurant(String restaurantName, String address, long operatingHours, Type type){
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "restaurant_id")
+    private Long restaurantId;
+
+    public Restaurant(String restaurantName, String address, long operatingHours, Type type, Long restaurantId){
 
         this.restaurantName = restaurantName;
         this.address = address;
         this.operatingHours = operatingHours;
         this.type = type;
+        this.restaurantId = restaurantId;
 
+    }
+
+    public Long getRestaurantId()
+    {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(Long restaurantId)
+    {
+        this.restaurantId = restaurantId;
     }
 
     public String getRestaurantName()
