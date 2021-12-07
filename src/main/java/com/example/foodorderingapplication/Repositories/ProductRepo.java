64f31d00c6f690fd86fac1f8627504b3db95c909
7laslan <73464685+7laslan@users.noreply.github.com>
2021-12-07 17:52:27 +0200

    Update ProductRepo.java

--- src/main/java/com/example/foodorderingapplication/Repositories/ProductRepo.java
+++ src/main/java/com/example/foodorderingapplication/Repositories/ProductRepo.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepo extends JpaRepository<Product, String> {
+public interface ProductRepo extends JpaRepository<Product, Long> {
 
     Product findProductById(Long productId);
 
