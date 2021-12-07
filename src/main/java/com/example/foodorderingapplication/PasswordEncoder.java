21ea88582ddf67f483a18259e45c3e487715fe87
7laslan <73464685+7laslan@users.noreply.github.com>
2021-12-07 14:22:55 +0200

    Update PasswordEncoder.java

--- src/main/java/com/example/foodorderingapplication/PasswordEncoder.java
+++ src/main/java/com/example/foodorderingapplication/PasswordEncoder.java
@@ -1,4 +1,4 @@
-package com.example.foodorderingapplication;
+/*package com.example.foodorderingapplication;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -6,4 +6,4 @@ import org.springframework.stereotype.Service;
 @Service
 public class PasswordEncoder extends BCryptPasswordEncoder {
 
-}
+}*/
