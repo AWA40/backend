0840d6f65d297d8c4471261a8105b0d754d49b8a
7laslan <73464685+7laslan@users.noreply.github.com>
2021-12-08 12:55:33 +0200

    Update CustomerRepo.java

--- src/main/java/com/example/foodorderingapplication/Repositories/CustomerRepo.java
+++ src/main/java/com/example/foodorderingapplication/Repositories/CustomerRepo.java
@@ -4,12 +4,14 @@ import com.example.foodorderingapplication.Customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
 
     //Customer findCustomerByIdd(Long customerId);
     
+    @Query
     Customer findByUsername(String username);
 
 }
