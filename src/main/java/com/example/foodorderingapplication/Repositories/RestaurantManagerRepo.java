50eecbae419f28e3a703676a8be106ba2c0c373c
7laslan <73464685+7laslan@users.noreply.github.com>
2021-12-08 12:55:50 +0200

    Update RestaurantManagerRepo.java

--- src/main/java/com/example/foodorderingapplication/Repositories/RestaurantManagerRepo.java
+++ src/main/java/com/example/foodorderingapplication/Repositories/RestaurantManagerRepo.java
@@ -4,12 +4,14 @@ import com.example.foodorderingapplication.RestaurantManager;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface RestaurantManagerRepo extends JpaRepository<RestaurantManager, Long> {
 
     //RestaurantManager findManagerById(Long adminId);
     
+    @Query
     RestaurantManager findByUsername(String username);
     
 }
